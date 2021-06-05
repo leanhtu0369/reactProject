742de00e3cbfe41814975e7866c34a2921b2dc33
LeTu <leanhtu0369@gmail.com>
2021-06-05 20:49:25 +0700

    push all

--- src/App.js
+++ src/App.js
@@ -1,25 +1,315 @@
-import logo from './logo.svg';
+import { faPause, faPlay, faRandom, faRedo, faStepBackward, faStepForward } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useEffect, useRef, useState } from 'react';
+
+import { songs as songsData } from "./mockData/songs"
 import './App.css';
 
-function App() {
+const App = () => {
+  const sizeCdDefault = 200
+
+  const [songs, setSongs] = useState(songsData)
+  const [cd, setCd] = useState({
+    size: sizeCdDefault,
+    opacity: 1
+  })
+  const [limitIndexSong, setLimitIndexSong] = useState({
+    min: 0,
+    max: songs.length - 1
+  })
+  const [currentSong, setCurrentSong] = useState({
+    index: limitIndexSong.min,
+    isPlaying: false,
+    timeLine: 0
+  })
+  const [songRepeat, setSongRepeat] = useState(false)
+  const [songRandom, setSongRandom] = useState(false)
+  const [cdThumbAnimate, setCdThumbAnimate] = useState({})
+  const [volume, setVolume] = useState(100)
+  const [storageSongs, setStorageSongs] = useState([limitIndexSong.min])
+
+  const cdThumb = useRef();
+  const btnNext = useRef();
+  const inputAudio = useRef();
+  const didUpdate = useRef(false);
+ 
+  const handlePlay = () => {
+    cdThumbAnimate.play()
+    setCurrentSong({
+      ...currentSong,
+      isPlaying: true
+    })
+  }
+
+  const handlePause = () => {
+    cdThumbAnimate.pause()
+    setCurrentSong({
+      ...currentSong,
+      isPlaying: false
+    })
+  }
+
+  const onTogglePlay = () => {
+    currentSong.isPlaying ? inputAudio.current.pause() : inputAudio.current.play()
+  }
+
+  const onChooseSong = (id) => {
+    if (id !== currentSong.index) {
+      setCurrentSong({
+        ...currentSong,
+        index: id,
+        isPlaying: true,
+        timeLine: 0
+      })
+    }
+  }
+
+  const onNextSong = () => {
+    if (songRandom) {
+      handleRandomSong()
+    } else {
+      setCurrentSong({
+        ...currentSong,
+        index: currentSong.index < limitIndexSong.max ? currentSong.index + 1 : 0
+      })
+    }
+  }
+
+  const onPrevSong = () => {
+    if (songRandom) {
+      handleRandomSong()
+    } else {
+      setCurrentSong({
+        ...currentSong,
+        index: currentSong.index === 0 ? limitIndexSong.max : currentSong.index - 1
+      })
+    }
+  }
+
+  const onRepeat = () => {
+    setSongRepeat(!songRepeat)
+  }
+
+  const onRandom = () => {
+    setSongRandom(!songRandom)
+  }
+
+  const handleRandomSong = () => {
+    let newIndex;
+    do {
+      newIndex = Math.floor(Math.random() * songs.length)
+    } while (newIndex === currentSong.index || storageSongs.some(song => song === newIndex))
+
+    if(storageSongs.length === limitIndexSong.max) {
+      setStorageSongs([])
+    } else {
+      setStorageSongs([
+        ...storageSongs,
+        newIndex
+      ]) 
+    }
+
+    setCurrentSong({
+      ...currentSong,
+      index: newIndex
+    })
+  }
+
+  const handleTimeLineSong = () => {
+    if (inputAudio.current.duration) {
+      setCurrentSong({
+        ...currentSong,
+        timeLine: Math.floor((inputAudio.current.currentTime / inputAudio.current.duration) * 100)
+      })
+    }
+  }
+
+  const handleEndedSong  = () => {
+    if (!songRepeat) {
+      btnNext.current.click()
+    }
+  }
+
+  const onChangeTimeLineSong = e => {
+    const seekto = inputAudio.current.duration * (+e.target.value / 100);
+    inputAudio.current.currentTime = seekto;
+    setCurrentSong({
+      ...currentSong,
+      timeLine: Math.floor(e.target.value)
+    })
+  }
+
+  const onChangeVolume = e => {
+    setVolume(+e.target.value)
+    inputAudio.current.volume = +e.target.value / 100
+  }
+
+  useEffect(() => {
+
+  })
+  
+  useEffect(() => {
+    // inputAudio.current.controls = true
+    setCdThumbAnimate(
+      cdThumb.current.animate([{
+        transform: "rotate(360deg)"
+      }], {
+        duration: 10000,
+        iterations: Infinity
+      })
+    )
+  }, [])
+
+  useEffect(() => {
+    if (didUpdate.current) {
+      cdThumbAnimate.pause()
+    }
+  }, [cdThumbAnimate])
+
+  useEffect(() => {
+    if (didUpdate.current) {
+      inputAudio.current.play()
+    } else {
+      didUpdate.current = true
+    }
+  }, [currentSong.index])
+
+  useEffect(() => {
+    if (songRepeat) {
+      inputAudio.current.loop = true
+    } else {
+      inputAudio.current.loop = false
+    }
+  }, [songRepeat])
+
+  document.onscroll = () => {
+    const newSizeCd = sizeCdDefault - window.scrollY
+    setCd({
+      size: newSizeCd > 0 ? newSizeCd : 0,
+      opacity: newSizeCd > 0 ? newSizeCd / sizeCdDefault : 0
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className={`player ${currentSong.isPlaying ? 'playing' : ''}`}>
+        {/* Dashboard */}
+        <div className="dashboard">
+          {/* Header */}
+          <header>
+            <h4>Now playing:</h4>
+            <h2>{songs[currentSong.index].name}</h2>
+          </header>
+
+          {/* CD */}
+          <div className="cd" style={{ width: cd.size, opacity: cd.opacity }}>
+            <div 
+              className="cd-thumb" 
+              style={{ backgroundImage: `url(${songs[currentSong.index].image})` }}
+              ref={cdThumb}
+            >
+            </div>
+          </div>
+
+          {/* Control */}
+          <div className="control">
+            <div 
+              className={`btn btn-repeat ${songRepeat ? 'active' : ''}`}
+              onClick={onRepeat}
+            >
+              <FontAwesomeIcon icon={faRedo} />
+            </div>
+            <div 
+              className="btn btn-prev"
+              onClick={onPrevSong}
+            >
+              <FontAwesomeIcon icon={faStepBackward} />
+              <i className="fas fa-step-backward"></i>
+            </div>
+            <div 
+              className={`btn btn-toggle-play`}
+              onClick={onTogglePlay}
+            >
+              <FontAwesomeIcon icon={faPause} className="icon-pause" />
+              <FontAwesomeIcon icon={faPlay} className="icon-play"  />
+            </div>
+            <div 
+              className="btn btn-next"
+              ref={btnNext}
+              onClick={onNextSong}
+            >
+              <FontAwesomeIcon icon={faStepForward} />
+            </div>
+            <div 
+              className={`btn btn-random ${songRandom ? 'active' : ''}`}
+              onClick={onRandom}
+            >
+              <FontAwesomeIcon icon={faRandom} />
+            </div>
+            <div className="">
+              <input 
+                id="volume" 
+                className="volume" 
+                type="range" 
+                value={volume}
+                step={1} 
+                min={0} 
+                max={100} 
+                onChange={onChangeVolume}
+              />
+            </div>
+          </div>
+
+          <input 
+            id="progress" 
+            className="progress" 
+            type="range" 
+            value={currentSong.timeLine}
+            step={1} 
+            min={0} 
+            max={100} 
+            onChange={onChangeTimeLineSong}
+          />
+
+          <audio 
+            id="audio" 
+            ref={inputAudio} 
+            src={`songs/${songs[currentSong.index].path}`}
+            onPlay={handlePlay}
+            onPause={handlePause}
+            onTimeUpdate={handleTimeLineSong}
+            onEnded={handleEndedSong}
+          >
+
+          </audio>
+        </div>
+
+        {/* Playlist */}
+        <div className="playlist">
+          {
+            songs.map((song, index) => (
+              <div 
+                key={index}
+                className={`song ${index === currentSong.index ? 'active' : ''}`}
+                onClick={() => onChooseSong(index)}
+              >
+                <div className="thumb" style={{ backgroundImage: `url(${song.image})`}}></div>
+
+                <div className="body">
+                  <h3 className="title">{song.name}</h3>
+                  <p className="author">{song.singer}</p>
+                </div>
+
+                <div className="option">
+                  <i className="fas fa-ellipsis-h"></i>
+                </div>
+              </div>
+            ))
+          }
+        </div>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/mockData/songs.js
@@ -0,0 +1,238 @@
+export const songs = [
+  {
+    name: "Unbreak My Heart",
+    singer: "Toni Braxton",
+    path: "song1.mp3",
+    image: "https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/covers/7/c/7c11f23682dbbc2b1efa2be8224d9679_1300205918.jpg"
+  },
+
+  {
+    name: "Lemon Tree",
+    singer: "Fools Garden",
+    path: "song2.mp3",
+    image: "https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/avatars/4/b/4bc920ca89933140af683ace942e3e91_1288098219.jpg"
+  },
+
+  {
+    name: "Because You Loved Me (Theme from \"Up Close and Personal\")",
+    singer: "Céline Dion",
+    path: "song3.mp3",
+    image: "https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/cover/6/9/4/c/694c1f1d4cd3730fe8cc7145cd587372.jpg"
+  },
+
+  // {
+  //   name: "Beat It",
+  //   singer: "Michael Jackson",
+  //   path: "song4.mp3",
+  //   image: "https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/covers/e/5/e5970e5377fd83e1303b73058fd29cd0_1289100148.jpg"
+  // },
+
+  // {
+  //   name: "Seasons in the Sun",
+  //   singer: "Westlife",
+  //   path: "song5.mp3",
+  //   image: "https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/cover/6/8/4/d/684d18a08e08aff43bd298cab5208022.jpg"
+  // },
+
+  // {
+  //   name: "...Baby One More Time",
+  //   singer: "Britney Spears",
+  //   path: "song6.mp3",
+  //   image: "https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/covers/0/c/0cba771f043663ab677be1404d1dd84a_1288684476.jpg"
+  // },
+
+  // {
+  //   name: "You Are Not Alone.",
+  //   singer: "Michael Jackson",
+  //   path: "song7.mp3",
+  //   image: "https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/covers/2/c/2c9cc6b64aee4732b262c7e9c2d79aae_1289537693.jpg"
+  // },
+
+  // {
+  //   name: "Say You Will",
+  //   singer: "Tokyo Square",
+  //   path: "song8.mp3",
+  //   image: "https://photo-resize-zmp3.zadn.vn/w240_r1x1_png/avatars/6/a/6af24c2e33083165083e727f5d340def_1325650380.png"
+  // },
+
+  // {
+  //   name: "Imagine",
+  //   singer: "John Lennon",
+  //   path: "song9.mp3",
+  //   image: "https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/covers/1/a/1a3a4230b340f9fbea942acd6004b67f_1291573112.jpg"
+  // },
+
+  // {
+  //   name: "Careless Whisper",
+  //   singer: "Wham!",
+  //   path: "song10.mp3",
+  //   image: "https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/covers/0/6/06a7b009242a6ee108b235f58642be9b_1299126182.jpg"
+  // },
+
+  // {
+  //   name: "Right Here Waiting For You",
+  //   singer: "Richard Marx",
+  //   path: "song11.mp3",
+  //   image: "https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/avatars/0/1/0150dd24d4e5ee7f9dd35997a3b7a00d_1327739011.jpg"
+  // },
+
+  // {
+  //   name: "Yesterday",
+  //   singer: "The Beatles",
+  //   path: "song12.mp3",
+  //   image: "https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/avatars/7/5/75286db109298a5f1869e89f4df20606_1287385498.jpg"
+  // },
+
+  // {
+  //   name: "Heal The World",
+  //   singer: "Michael Jackson",
+  //   path: "song13.mp3",
+  //   image: "https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/covers/8/d/8defcd9de7e738c3dfb041934a2efb2b_1289034638.jpg"
+  // },
+
+  // {
+  //   name: "Love You More Than I Can Say",
+  //   singer: "Leo Sayer",
+  //   path: "song14.mp3",
+  //   image: "https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/avatars/0/f/0f1ae400bf19a6680584fd59dd275816_1336241271.jpg"
+  // },
+
+  // {
+  //   name: "(They Long To Be) Close To You",
+  //   singer: "Every Little Thing",
+  //   path: "song15.mp3",
+  //   image: "https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/covers/4/f/4f3a4d97e23ab15d94ede295579d3306_1289738529.jpg"
+  // }, 
+  // {
+  //   name: "Hotel California (Remastered)",
+  //   singer: "Eagles",
+  //   path: "song16.mp3",
+  //   image: "https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/covers/0/0/004a999b45462a2a09e21d188762f36c_1511978136.jpg"
+  // },
+
+  // {
+  //   name: "How Deep Is Your Love",
+  //   singer: "The Bee Gees",
+  //   path: "song17.mp3",
+  //   image: "https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/covers/2/e/2e8a5c31a68e7fdd9e00afd6a93ff99b_1286274973.jpg"
+  // },
+
+  // {
+  //   name: "My Heart Will Go On (Love Theme from \"Titanic\")",
+  //   singer: "Céline Dion",
+  //   path: "song18.mp3",
+  //   image: "https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/cover/a/f/c/9/afc9ef6d83089a96568b0a875ad8f67f.jpg"
+  // },
+
+  // {
+  //   name: "Time After Time",
+  //   singer: "Cyndi Lauper",
+  //   path: "song19.mp3",
+  //   image: "https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/covers/6/c/6cde95ca794ee94d0236f6a574b1c9ee_1357786466.jpg"
+  // },
+
+  // {
+  //   name: "We Are The World",
+  //   singer: "USA for Africa",
+  //   path: "song20.mp3",
+  //   image: "https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/covers/8/9/89fbb26dbf3b8d21f91882a9e20d2479_1372745279.jpg"
+  // },
+
+  // {
+  //   name: "Woman In Love",
+  //   singer: "Barbra Streisand",
+  //   path: "song21.mp3",
+  //   image: "https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/covers/9/b/9b98361426d08b03bde4eef2659275b7_1355817484.jpg"
+  // },
+
+  // {
+  //   name: "Eternal Flame",
+  //   singer: "Human Nature",
+  //   path: "song22.mp3",
+  //   image: "https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/cover/2/7/8/d/278dcdf201556999abcd3b4c4bfb1df9.jpg"
+  // },
+
+  // {
+  //   name: "Without You",
+  //   singer: "Mariah Carey",
+  //   path: "song23.mp3",
+  //   image: "https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/covers/e/2/e2a1b8aba64998495f14ca3c955c54aa_1431663877.jpg"
+  // },
+
+  // {
+  //   name: "Hard to Say I'm Sorry",
+  //   singer: "Chicago",
+  //   path: "song24.mp3",
+  //   image: "https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/covers/2/e/2ea7d2182ab9505af3892709364d2424_1321129350.jpg"
+  // },
+
+  // {
+  //   name: "I Will Always Love You",
+  //   singer: "Whitney Houston",
+  //   path: "song25.mp3",
+  //   image: "https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/covers/b/1/b1ceee35ffb6d99237c7f9c0008d0e1a_1348241173.jpg"
+  // },
+
+  // {
+  //   name: "Casablanca",
+  //   singer: "Bertie Higgins",
+  //   path: "song26.mp3",
+  //   image: "https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/covers/e/e/ee3d51292b9964b5f06516d9956364c3_1340189534.jpg"
+  // },
+
+  // {
+  //   name: "As Long As You Love Me",
+  //   singer: "Backstreet Boys",
+  //   path: "song27.mp3",
+  //   image: "https://photo-resize-zmp3.zadn.vn/w240_r1x1_png/covers/9/f/9ffda008c2eca0b04e4e0a26430ab2e5_1321757224.png"
+  // },
+
+  // {
+  //   name: "Another Day In Paradise",
+  //   singer: "Phil Collins",
+  //   path: "song28.mp3",
+  //   image: "https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/covers/b/0/b0174660573738ea0610771bc5209858_1355893989.jpg"
+  // },
+
+  // {
+  //   name: "How Am I Supposed To Live Without You.",
+  //   singer: "Michael Bolton",
+  //   path: "song29.mp3",
+  //   image: "https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/covers/0/6/06a7b009242a6ee108b235f58642be9b_1299126114.jpg"
+  // },
+
+  // {
+  //   name: "Take My Breath Away",
+  //   singer: "Berlin",
+  //   path: "song30.mp3",
+  //   image: "https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/covers/d/2/d2401309257edd878abe9b926cfa6f94_1343895502.jpg"
+  // },
+
+  // {
+  //   name: "Billie Jean",
+  //   singer: "Michael Jackson",
+  //   path: "song31.mp3",
+  //   image: "https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/cover/2/b/e/e/2bee50bebe005fd414a7dba404c6b0db.jpg"
+  // },
+
+  // {
+  //   name: "The Power Of Love",
+  //   singer: "Céline Dion",
+  //   path: "song32.mp3",
+  //   image: "https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/covers/1/2/12dba50a93502e028b47d68146bca782_1415075928.jpg"
+  // },
+
+  // {
+  //   name: "More Than Words",
+  //   singer: "Extreme",
+  //   path: "song33.mp3",
+  //   image: "https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/covers/7/5/75b6ef7206c06df33329b37a86a536e0_1385035244.jpg"
+  // },
+
+  // {
+  //   name: "My All",
+  //   singer: "Mariah Carey",
+  //   path: "song34.mp3",
+  //   image: "https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/covers/e/2/e2a1b8aba64998495f14ca3c955c54aa_1431663877.jpg"
+  // },
+]
